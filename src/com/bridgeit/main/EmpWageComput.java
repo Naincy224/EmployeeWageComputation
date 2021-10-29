81875831824b168ee30eeba3bf6bfe277fadce0f
Naincy224 <pihusingh224@gmail.com>
2021-10-29 08:37:00 +0530

    Adding employee welcome message program

---
+++ src/com/bridgeit/main/EmpWageComput.java
@@ -0,0 +1,11 @@
+package com.bridgeit.main;
+
+public class EmpWageComput {
+	
+	
+	public void printWelcomeMessage () {
+		
+		System.out.println("Welcome to Employee Wage Computation Program");
+	
+	}
+}
---
+++ src/com/bridgeit/main/Mainclass.java
@@ -0,0 +1,14 @@
+package com.bridgeit.main;
+
+public class Mainclass {
+	
+	
+public static void main(String[] args) {
+		
+	EmpWageComput empObj= new EmpWageComput();
+	
+	empObj.printWelcomeMessage();
+	
+	}
+
+}
