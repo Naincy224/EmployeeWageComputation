da740a918915cb91d585989eff1753b23dc5dba2
Naincy224 <pihusingh224@gmail.com>
2021-10-29 09:45:46 +0530

    adding UC1 for adding presentAbsent funcationality

--- src/com/bridgeit/main/EmpWageComput.java
+++ src/com/bridgeit/main/EmpWageComput.java
@@ -1,11 +1,25 @@
 package com.bridgeit.main;
 
 public class EmpWageComput {
-	
-	
-	public void printWelcomeMessage () {
-		
+
+	public void printWelcomeMessage() {
+
 		System.out.println("Welcome to Employee Wage Computation Program");
-	
+
 	}
+
+	// Adding presentAbsent functionality for UC1
+	public String presentOrAbsent() {
+		double randomNum = Math.floor(Math.random() * 10) % 2;
+		String absentPresent;
+		if (randomNum == 0) {
+			absentPresent = "absent";
+		} else {
+			absentPresent = "present";
+
+		}
+
+		return absentPresent;
+	}
+
 }
--- src/com/bridgeit/main/Mainclass.java
+++ src/com/bridgeit/main/Mainclass.java
@@ -1,14 +1,14 @@
 package com.bridgeit.main;
 
 public class Mainclass {
-	
-	
-public static void main(String[] args) {
-		
-	EmpWageComput empObj= new EmpWageComput();
-	
-	empObj.printWelcomeMessage();
-	
+
+	public static void main(String[] args) {
+
+		EmpWageComput empObj = new EmpWageComput();
+
+		empObj.printWelcomeMessage();
+		String presentOrAbsent=empObj.presentOrAbsent();
+		System.out.println("The employee is : "+presentOrAbsent);
 	}
 
 }
